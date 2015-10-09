d038b59898a5d1a1e1cf2dc265c7ff4efc91be37
Chan <@purdue.edu>
2015-10-09 10:39:47 -0400

    Added an exponent function

--- calc.py
+++ calc.py
@@ -33,6 +33,11 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Exp function
+# a -- multiplicand
+# b -- multiplier
+def exp(a, b):
+    return a ** b
 
 # -------------------------------------------------------- #
 
@@ -67,6 +72,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+		elif (op == "**"):
+            print "Exponent: ", div(a, b)
         else:
             print "Invalid operation..."
 
